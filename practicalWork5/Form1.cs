a37a68ec4fdcfab3fc07775c3580d6196e34d954
Gamer228Mok1 <kwelov.work@gmail.com>
2024-10-20 18:57:08 +0300

    Сделал

--- practicalWork5/Form1.cs
+++ practicalWork5/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using practicalWork5.BuilderBurger;
+using practicalWork5.DBCon;
 
 namespace practicalWork5
 {
@@ -15,6 +17,39 @@ namespace practicalWork5
         public Form1()
         {
             InitializeComponent();
+        }
+        Model1 model = new Model1();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BurgerBuilder burgerBuilder = new BurgerBuilder();
+            BurgerDirector burgerDirector = new BurgerDirector(burgerBuilder);
+
+            if (ComboBoxBurger.SelectedItem.ToString() == "Бургер стандартный")
+                burgerDirector.BuildDefault();
+            else
+                burgerDirector.BuildWithBeacon();
+
+            try
+            {
+                model.Burgers.Add(burgerBuilder.GetBurgers());
+                model.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            LoadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+        private void LoadData()
+        {
+            ComboBoxBurger.SelectedIndex = 0;
+            dataGridView1.DataSource = model.Burgers.ToList();
         }
     }
 }
